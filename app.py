18e9f1e9db4fa0899ed6f55c2951007d6370b0b7
Luke Bray <lukisbray@gmail.com>
2021-05-12 15:31:28 -0700

    dynamic rendering

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask.helpers import url_for
 from werkzeug.utils import redirect
 import wikipedia
 from flask import Flask, request, render_template, redirect
-from waitress import serve
 
 app = Flask(__name__)
 
@@ -10,20 +9,18 @@ app = Flask(__name__)
 def search_input():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def search_result():
     text = request.form['text']
     search = wikipedia.search(text)
     first_article = search[0]
     summary = wikipedia.summary(first_article, sentences=10)
-    return summary
-
+    result = summary
+    return render_template("index.html", result=result)
 
 
 if __name__ == '__main__':
-    # app.run()
-    serve(app)
-
+    app.run()
 
 
 
