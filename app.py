3816d808d69944c82482154332b7e016383a49e7
Luke Bray <lukisbray@gmail.com>
2021-05-24 14:41:23 -0700

    Connect w/ Jacob

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask.helpers import url_for
 from werkzeug.utils import redirect
+from werkzeug.wrappers import Response
 import wikipedia
-from flask import Flask, request, render_template, redirect
+import requests
+from flask import *
 
 app = Flask(__name__)
+
+
+r = requests.get('https://translator-osu.herokuapp.com/translate-to?text=hello&language=French')
+print(r.text)
+print(r.status_code)
 
 @app.route('/')
 def search_input():
     return render_template('index.html')
+
 
 @app.route('/', methods=['POST', 'GET'])
 def search_result():
@@ -16,7 +24,20 @@ def search_result():
     first_article = search[0]
     summary = wikipedia.summary(first_article, sentences=10)
     result = summary
-    return render_template("index.html", result=result)
+    url = (f'https://translator-osu.herokuapp.com/translate-to?text={summary}&language=French')
+    response = requests.get(url)
+    return render_template("index.html", result=result, response=response.text)
+
+
+@app.route('/scrape', methods=['POST', 'GET'])
+def scrape_info():
+    take = request.args.get('take')
+    search = wikipedia.search(take)
+    first_article = search[0]
+    summary = wikipedia.summary(first_article, sentences=10)
+    return (summary)
+
+
 
 
 if __name__ == '__main__':
